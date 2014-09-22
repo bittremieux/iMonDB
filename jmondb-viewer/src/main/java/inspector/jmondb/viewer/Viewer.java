3ed5555e5cbfb13bd4c9b6aeabffce958a62c228
Wout Bittremieux <bittremieux@gmail.com>
2014-09-22 09:01:37 +0200

    Improve graph creation

--- jmondb-viewer/src/main/java/inspector/jmondb/viewer/Viewer.java
+++ jmondb-viewer/src/main/java/inspector/jmondb/viewer/Viewer.java
@@ -12,6 +12,8 @@ import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.ValueMarker;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYDifferenceRenderer;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
@@ -507,40 +509,29 @@ public class Viewer extends JPanel {
 								minSeries.add(value.getFromRun().getSampleDate().getTime(), value.getMin());
 								maxSeries.add(value.getFromRun().getSampleDate().getTime(), value.getMax());
 							}
-							XYSeriesCollection q1Collection = new XYSeriesCollection();
-							q1Collection.addSeries(medianSeries);
-							q1Collection.addSeries(q1Series);
-							XYSeriesCollection q3Collection = new XYSeriesCollection();
-							q3Collection.addSeries(medianSeries);
-							q3Collection.addSeries(q3Series);
-							XYSeriesCollection minCollection = new XYSeriesCollection();
-							minCollection.addSeries(q1Series);
-							minCollection.addSeries(minSeries);
-							XYSeriesCollection maxCollection = new XYSeriesCollection();
-							maxCollection.addSeries(q3Series);
-							maxCollection.addSeries(maxSeries);
+
+							XYSeriesCollection medianCollection = new XYSeriesCollection(medianSeries);
+							XYSeriesCollection q1q3Collection = new XYSeriesCollection();
+							q1q3Collection.addSeries(q1Series);
+							q1q3Collection.addSeries(q3Series);
+							XYSeriesCollection minMaxCollection = new XYSeriesCollection();
+							minMaxCollection.addSeries(minSeries);
+							minMaxCollection.addSeries(maxSeries);
 
 							// renderer
-							XYDifferenceRenderer q1Renderer = new XYDifferenceRenderer(Color.GRAY, Color.GRAY, true);
-							q1Renderer.setSeriesPaint(0, Color.BLACK);
-							q1Renderer.setSeriesPaint(1, Color.GRAY);
-							q1Renderer.setSeriesShape(0, new Ellipse2D.Double(-2, -2, 4, 4));
-							q1Renderer.setSeriesShape(1, new Ellipse2D.Double(-2, -2, 4, 4));
-							XYDifferenceRenderer q3Renderer = new XYDifferenceRenderer(Color.GRAY, Color.GRAY, true);
-							q3Renderer.setSeriesPaint(0, Color.BLACK);
-							q3Renderer.setSeriesPaint(1, Color.GRAY);
-							q3Renderer.setSeriesShape(0, new Ellipse2D.Double(-2, -2, 4, 4));
-							q3Renderer.setSeriesShape(1, new Ellipse2D.Double(-2, -2, 4, 4));
-							XYDifferenceRenderer minRenderer = new XYDifferenceRenderer(Color.LIGHT_GRAY, Color.LIGHT_GRAY, true);
-							minRenderer.setSeriesPaint(0, Color.GRAY);
-							minRenderer.setSeriesPaint(1, Color.LIGHT_GRAY);
-							minRenderer.setSeriesShape(0, new Ellipse2D.Double(-2, -2, 4, 4));
-							minRenderer.setSeriesShape(1, new Ellipse2D.Double(-2, -2, 4, 4));
-							XYDifferenceRenderer maxRenderer = new XYDifferenceRenderer(Color.LIGHT_GRAY, Color.LIGHT_GRAY, true);
-							maxRenderer.setSeriesPaint(0, Color.GRAY);
-							maxRenderer.setSeriesPaint(1, Color.LIGHT_GRAY);
-							maxRenderer.setSeriesShape(0, new Ellipse2D.Double(-2, -2, 4, 4));
-							maxRenderer.setSeriesShape(1, new Ellipse2D.Double(-2, -2, 4, 4));
+							XYItemRenderer medianRenderer = new XYLineAndShapeRenderer();
+							medianRenderer.setSeriesPaint(0, Color.BLACK);
+							medianRenderer.setSeriesShape(0, new Ellipse2D.Double(-2, -2, 4, 4));
+							XYDifferenceRenderer q1q3Renderer = new XYDifferenceRenderer(Color.GRAY, Color.GRAY, true);
+							q1q3Renderer.setSeriesPaint(0, Color.GRAY);
+							q1q3Renderer.setSeriesPaint(1, Color.GRAY);
+							q1q3Renderer.setSeriesShape(0, new Ellipse2D.Double(-2, -2, 4, 4));
+							q1q3Renderer.setSeriesShape(1, new Ellipse2D.Double(-2, -2, 4, 4));
+							XYDifferenceRenderer minMaxRenderer = new XYDifferenceRenderer(Color.LIGHT_GRAY, Color.LIGHT_GRAY, true);
+							minMaxRenderer.setSeriesPaint(0, Color.LIGHT_GRAY);
+							minMaxRenderer.setSeriesPaint(1, Color.LIGHT_GRAY);
+							minMaxRenderer.setSeriesShape(0, new Ellipse2D.Double(-2, -2, 4, 4));
+							minMaxRenderer.setSeriesShape(1, new Ellipse2D.Double(-2, -2, 4, 4));
 
 							// create axis
 							DateAxis dateAxis = new DateAxis("Date");
@@ -554,14 +545,12 @@ public class Viewer extends JPanel {
 							XYPlot plot = new XYPlot();
 							plot.setDomainAxis(dateAxis);
 							plot.setRangeAxis(valueAxis);
-							plot.setDataset(0, q1Collection);
-							plot.setDataset(1, q3Collection);
-							plot.setDataset(2, minCollection);
-							plot.setDataset(3, maxCollection);
-							plot.setRenderer(0, q1Renderer);
-							plot.setRenderer(1, q3Renderer);
-							plot.setRenderer(2, minRenderer);
-							plot.setRenderer(3, maxRenderer);
+							plot.setDataset(0, medianCollection);
+							plot.setDataset(1, q1q3Collection);
+							plot.setDataset(2, minMaxCollection);
+							plot.setRenderer(0, medianRenderer);
+							plot.setRenderer(1, q1q3Renderer);
+							plot.setRenderer(2, minMaxRenderer);
 							JFreeChart chart = new JFreeChart(valueName, plot);
 							chart.setBackgroundPaint(Color.WHITE);
 							chartPanel = new ChartPanel(chart, false, true, false, true, false);
