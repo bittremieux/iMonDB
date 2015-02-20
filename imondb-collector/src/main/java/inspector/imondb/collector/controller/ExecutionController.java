b63ad2f837be788cbfe693713ec4f654a3cc2c27
Wout Bittremieux <bittremieux@gmail.com>
2015-02-20 17:04:13 +0100

    Compile GUI designer forms into code to build with Maven

--- imondb-collector/src/main/java/inspector/imondb/collector/controller/ExecutionController.java
+++ imondb-collector/src/main/java/inspector/imondb/collector/controller/ExecutionController.java
@@ -1,5 +1,25 @@
 package inspector.imondb.collector.controller;
 
+/*
+ * #%L
+ * iMonDB Collector
+ * %%
+ * Copyright (C) 2014 - 2015 InSPECtor
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import inspector.imondb.collector.model.config.Configuration;
 import inspector.imondb.collector.view.gui.overview.ProgressPanel;
 
