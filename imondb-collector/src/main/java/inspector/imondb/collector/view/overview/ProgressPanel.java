0e89ef30b61edb2615624a358b2318d328ec654d
Wout Bittremieux <bittremieux@gmail.com>
2015-02-20 14:07:59 +0100

    Print error messages in red and avoid line wrapping

--- imondb-collector/src/main/java/inspector/imondb/collector/view/overview/ProgressPanel.java
+++ imondb-collector/src/main/java/inspector/imondb/collector/view/overview/ProgressPanel.java
@@ -5,6 +5,11 @@ import inspector.imondb.collector.controller.ExecutionController;
 import inspector.imondb.collector.view.CollectorFrame;
 
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import java.awt.*;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -13,7 +18,7 @@ public class ProgressPanel {
 
     private JPanel panel;
 
-    private JTextArea textAreaLog;
+    private JTextPane textPaneLog;
     private JProgressBar progressBar;
 
     private ExecutionPanel executionPanel;
@@ -28,11 +33,47 @@ public class ProgressPanel {
         this.collectorFrame = collectorFrame;
         this.executionController = executionController;
 
-        textAreaLog.setEditable(false);
+        textPaneLog.setEditable(false);
+        textPaneLog.getDocument().addDocumentListener(new LimitLinesDocumentListener(50));
         redirectSystemStreams();
 
         progressBar.setMaximum(100);
         progressBar.setStringPainted(true);
+    }
+
+    // http://unserializableone.blogspot.com/2009/01/redirecting-systemout-and-systemerr-to.html
+    private void redirectSystemStreams() {
+        Style styleOut = textPaneLog.addStyle("out", null);
+        StyleConstants.setForeground(styleOut, Color.BLACK);
+        OutputStream osOut = new OutputStream() {
+            @Override
+            public void write(final int b) throws IOException {
+                updateTextPane(String.valueOf((char) b), styleOut);
+            }
+        };
+        Style styleErr = textPaneLog.addStyle("err", styleOut);
+        StyleConstants.setForeground(styleErr, Color.RED);
+        OutputStream osErr = new OutputStream() {
+            @Override
+            public void write(int b) throws IOException {
+                updateTextPane(String.valueOf((char) b), styleErr);
+            }
+        };
+
+        System.setOut(new PrintStream(osOut, true));
+        System.setErr(new PrintStream(osErr, true));
+    }
+
+    private void updateTextPane(final String text, Style style) {
+        SwingUtilities.invokeLater(() -> {
+            Document doc = textPaneLog.getDocument();
+            try {
+                doc.insertString(doc.getLength(), text, style);
+            } catch(BadLocationException e) {
+                throw new RuntimeException(e);
+            }
+            textPaneLog.setCaretPosition(doc.getLength());
+        });
     }
 
     public JPanel getPanel() {
@@ -65,32 +106,5 @@ public class ProgressPanel {
     public void done() {
         executionPanel.getButton().setText("Start collector");
         collectorFrame.setWaitCursor(false);
-    }
-
-    // http://unserializableone.blogspot.com/2009/01/redirecting-systemout-and-systemerr-to.html
-    private void updateTextArea(final String text) {
-        SwingUtilities.invokeLater(() -> textAreaLog.append(text));
-    }
-
-    private void redirectSystemStreams() {
-        OutputStream out = new OutputStream() {
-            @Override
-            public void write(int b) throws IOException {
-                updateTextArea(String.valueOf((char) b));
-            }
-
-            @Override
-            public void write(byte[] b, int off, int len) throws IOException {
-                updateTextArea(new String(b, off, len));
-            }
-
-            @Override
-            public void write(byte[] b) throws IOException {
-                write(b, 0, b.length);
-            }
-        };
-
-        System.setOut(new PrintStream(out, true));
-        System.setErr(new PrintStream(out, true));
     }
 }
